2b67b62081080f20cd677bfdaa7db30bba89718d
NCP08 <navercloudplatform08.gmail.com>
2024-05-17 14:20:24 +0900

    ch04까지 수업 내용

---
+++ React_Exe/ch03_jsx/src/ex06_classname/Child.js
@@ -0,0 +1,10 @@
+import './Child.css';
+
+function Child() {
+  const name = "리액트";
+
+
+  return <div className="react">{name}</div>;
+}
+
+export default Child;
---
+++ React_Exe/ch04_component/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+// import MyComponent from './ex06_default_props copy/MyComponent';
+// import Child from './ex01_function/Child';
+// import Child from './ex02_class/Child';
+// import MyComponent from './ex03_props/MyComponent';
+// import Library from './ex04_props/Library';
+// import MyComponent from './ex05_props/MyComponent';
+// import MyComponent from './ex06_default_props/MyComponent';
+
+import MyComponent from './ex07_children/MyComponent';
+
+function App() {
+  return (
+    // <Library title='조선 선비들의 '/>
+  // <>
+  // <MyComponent />
+  // <MyComponent name="리액트" />
+  // </>
+  <MyComponent>리액트 프로그래밍</MyComponent>
+  );
+}
+
+export default App;
