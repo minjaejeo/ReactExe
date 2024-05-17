6ad0486166b5f241af8a9cdda668ba17db038cc2
NCP08 <navercloudplatform08.gmail.com>
2024-05-17 17:18:35 +0900

    ch04_comment까지 수업 내용

--- React_Exe/ch04_component/src/App.js
+++ React_Exe/ch04_component/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 // import MyComponent from './ex06_default_props copy/MyComponent';
 // import Child from './ex01_function/Child';
@@ -8,7 +8,19 @@ import './App.css';
 // import MyComponent from './ex05_props/MyComponent';
 // import MyComponent from './ex06_default_props/MyComponent';
 
-import MyComponent from './ex07_children/MyComponent';
+// import MyComponent from './ex07_children/MyComponent';
+
+// import MyComponent from './ex08_destructing/MyComponent';
+// import MyComponent from './ex09_prop_types/MyComponent';
+// import MyComponent from './ex10_prop_required/MyComponent';
+
+// import MyComponent from './ex11_prop_class/MyComponent';
+// import MyComponent from './ex12_prop_class/MyComponent';
+// import Team from './ex13_compose_component/Team';
+// import CommentList from './ex18_comment/CommentList';
+// import CommentList from './ex19_comment/CommentList';
+import CommentList from './ex20_comment/CommentList';
+
 
 function App() {
   return (
@@ -17,7 +29,16 @@ function App() {
   // <MyComponent />
   // <MyComponent name="리액트" />
   // </>
-  <MyComponent>리액트 프로그래밍</MyComponent>
+  // <MyComponent name="Happy Happy">리액트 프로그래밍</MyComponent>
+
+  // <MyComponent name="행운">리액트</MyComponent>
+  // <MyComponent name={3}>리액트</MyComponent>
+
+  // <MyComponent name="React_Programming">리액트</MyComponent>
+  // <MyComponent name="리액트_뷰_앵귤러_노드_넥스트" favoriteNumber={3}>리액트</MyComponent>
+
+  // <Team title="재능 기부 교류 서비스"/>
+  <CommentList/>
   );
 }
 
---
+++ React_Exe/ch04_component/src/ex13_compose_component/Student.jsx
@@ -0,0 +1,9 @@
+
+
+function Student({props}){
+    return(
+        <div>{props.name}은 {props.work}의 역할을 맡고 있습니다.</div>
+    );
+}
+
+export default Student;
