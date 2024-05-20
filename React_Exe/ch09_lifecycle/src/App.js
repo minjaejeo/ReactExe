7a7fe6a0d2b5c2b00d15570cc7ef7a83b77659c9
NCP08 <navercloudplatform08.gmail.com>
2024-05-20 17:29:18 +0900

    ch09_lifecycle 까지 수업 내용

---
+++ React_Exe/ch09_lifecycle/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+import NotificationList from './components/NotificationList';
+
+function App() {
+  return (
+    <NotificationList />
+  );
+}
+
+export default App;
