1d6504f9bd27ba8f9e7ac3b302bfc5a98eb10340
NCP08 <navercloudplatform08.gmail.com>
2024-05-28 16:00:19 +0900

    ch08_array_component 까지 복습

--- React_Exe/ch08_array_component/src/ex01_map/IterationSample.js
+++ React_Exe/ch08_array_component/src/ex01_map/IterationSample.js
@@ -1,7 +1,9 @@
+
+
 const IterationSample = () => {
-    const names = ['눈사람', '얼음', '눈', '바람'];
+    const names = ["눈사람", "얼음", "눈", "바람"];
     const nameList = names.map((name) => <li>{name}</li>);
-    return <ul>{nameList}</ul>
+    return <ul>{nameList}</ul>;
 };
 
 export default IterationSample;
