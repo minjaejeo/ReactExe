2ccac559d962e76af3cdfcd63052ed4a8dd1e8d2
NCP08 <navercloudplatform08.gmail.com>
2024-05-30 17:14:46 +0900

    ch15_shared_state까지 복습

--- React_Exe/ch15_shared_state/src/components/TemperatureInput.jsx
+++ React_Exe/ch15_shared_state/src/components/TemperatureInput.jsx
@@ -1,23 +1,21 @@
 
-
 const scaleNames = {
     c: '섭씨',
     f: '화씨'
 };
 
 function TemperatureInput(props){
-
     const { scale, temperature, onTemperatureChange} = props;
 
     const handleChange = (event) => {
         onTemperatureChange(event.target.value);
     }
-    return (
+    return(
         <fieldset>
             <legend>
-                온도를 입력해주세요(단위: {scaleNames[scale]});
+                온도를 입력해주세요(단위: {scaleNames[scale]})
             </legend>
-            <input value={temperature} onChange={handleChange}/>
+            <input value={temperature} onChange={handleChange} />
         </fieldset>
     );
 }
