a09ec3277c8d0cfc089945cca359ee9484e9706e
NCP08 <navercloudplatform08.gmail.com>
2024-06-03 16:29:45 +0900

    4/29 수업 내용

--- CardDatabase/src/main/java/org/mind/carddatabase/CardDatabaseApplication.java
+++ CardDatabase/src/main/java/org/mind/carddatabase/CardDatabaseApplication.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.mind.carddatabase.domain.Car;
 import org.mind.carddatabase.domain.Owner;
+import org.mind.carddatabase.domain.User;
 import org.mind.carddatabase.repository.CarRepository;
 import org.mind.carddatabase.repository.OwnerRepository;
+import org.mind.carddatabase.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,6 +22,7 @@ public class CardDatabaseApplication implements CommandLineRunner {
 
 	private final OwnerRepository ownerRepository;
 	private final CarRepository carRepository;
+	private final UserRepository userRepository;
 
 	public static void main(String[] args) {
 
@@ -44,6 +47,7 @@ public class CardDatabaseApplication implements CommandLineRunner {
 		Car car1 = Car.builder()
 				.brand("Ford")
 				.model("Mustang")
+				.color("white")
 				.registerNumber("AAA-111")
 				.year(2024)
 				.price(6400)
@@ -52,6 +56,7 @@ public class CardDatabaseApplication implements CommandLineRunner {
 		Car car2 = Car.builder()
 				.brand("Hyndai")
 				.model("Genesis")
+				.color("black")
 				.registerNumber("HHH-111")
 				.year(2024)
 				.price(8500)
@@ -60,6 +65,7 @@ public class CardDatabaseApplication implements CommandLineRunner {
 		Car car3 = Car.builder()
 				.brand("기아")
 				.model("쏘렌드")
+				.color("grey")
 				.registerNumber("SSS-111")
 				.year(2024)
 				.price(4300)
@@ -74,5 +80,23 @@ public class CardDatabaseApplication implements CommandLineRunner {
 		for(Car car : carRepository.findAll()){
 			log.info(car.toString());
 		}
+
+
+		// username=user / password=user
+		User user1 = User.builder()
+				.username("user")
+				.password("$2y$10$se7DoJ8j/IRjq50gfydWJeYqAnr2xz.WRX2wS4ID1Ye/pFkPFspSa")
+				.role("USER")
+				.build();
+
+		// username=admin / password=admin
+		User user2 = User.builder()
+				.username("admin")
+				.password("$2y$10$vuJXufg2CyLykCFP8SxKX.kLizaHM15AsxmRGCzgcMUfjYfndV36C")
+				.role("ADMIN")
+				.build();
+
+		userRepository.save(user1);
+		userRepository.save(user2);
 	}
 }
---
+++ CardDatabase/src/main/java/org/mind/carddatabase/domain/User.java
@@ -0,0 +1,28 @@
+package org.mind.carddatabase.domain;
+
+import lombok.*;
+import org.hibernate.annotations.Columns;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
